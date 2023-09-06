417f0542fcabbb714e9787f7650a64adc1448984
Bolphunga <aymendj@yahoo.com>
2023-09-06 05:20:04 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const body = document.querySelector("body")
+const p = document.createElement("p")
+const p1 = document.createElement("p")
+body.appendChild(p)
+body.appendChild(p1)
+
+const submitData = (name, email) => {
+ return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers:{
+        "Content-Type": 'application/json',
+        Accept: "application/json"
+        }, 
+    body : JSON.stringify({ name, email})
+        })
+        .then(r =>r.json())
+        .then(data=>{
+            p.textContent = data.id
+            console.log(data.id)})
+        .catch(error=>{
+            p1.textContent = error.message
+            alert("Something something error");
+            console.log(error.message)
+        })
+   
+}
